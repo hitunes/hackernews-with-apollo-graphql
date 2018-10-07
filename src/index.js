270e376ede190330904fa262253a76a502042b77
Itunu Adekoya <hitunes247@gmail.com>
2018-10-07 17:23:39 +0100

    made my first query and mutation in graphql

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//httpLink that will connect your ApolloClient instance with the GraphQL API, your GraphQL server will be running on http://localhost:4000.
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000"
+});
+// instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+//render the root component of your React app. The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop.
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
